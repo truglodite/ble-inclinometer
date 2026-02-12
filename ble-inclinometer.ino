72e21542eb5f7191b884cbaa05d59bd3669f35f6
truglodite <truglodite@users.noreply.github.com>
2026-02-11 20:35:46 -0800

    Update ble-inclinometer.ino
    Redo BLE server setup, use chars to send actual floats

--- ble-inclinometer.ino
+++ ble-inclinometer.ino
@@ -6,10 +6,10 @@
 
 // Usage:
 // Flash the board, power it on, and connect it to the app.
-// Subscribe to the roll (2c08) and pitch (2c09) sensors ("down/line arrow" buttons).
-// Configure the data as "signed int" ("quote" buttons).
-// To zero both axis, send a true boolean to the tare service  (up arrow on sensor w/ long uuid).
-// If using a battery for power via the battery pads, subscribe to the battery service to read battery %.
+// Subscribe to the roll (a68e) and pitch (d9bc) sensors ("down/line arrow" buttons).
+// Configure the data as "UTF-8" ("quote" buttons).
+// To zero both axis, send a true boolean (or 1) to the tare service (f7a7) (up arrow on sensor w/ long uuid).
+// If using a battery for power via the battery pads, subscribe to the battery service (5726) to read battery volts.
 
 // Arduino requires the seed gyro lib (not the 'duino download): https://github.com/Seeed-Studio/Seeed_Arduino_LSM6DS3
 
@@ -20,6 +20,7 @@
 #include <nrfx_saadc.h>
 #include <AnalogIn.h>
 #include <pinDefinitions.h>
+#include <avr/dtostrf.h>
 
 #define updateDelay 1000  // msec between data updates
 #define longFlash 2000   // msec to flash when tare is received
@@ -32,22 +33,29 @@
 #define batteryReadPin P0_14
 #define batteryAnalogPin P0_31
 LSM6DS3 myIMU(I2C_MODE, 0x6A);    //I2C device address 0x6A
+
+// Characteristic UUID's
+#define BLE_UUID_ANGLE_MONITOR_SERVICE "8acafa20-26e9-4d16-a792-cf7de147c01c"
+#define BLE_UUID_BATTERY_VOLTS  "5726c19a-8a75-5d7a-845d-aadf6734d7e7"
+#define BLE_UUID_ROLL_DEGREES  "a68e1ad6-8c88-56f4-b9d5-792af19cfb19"
+#define BLE_UUID_PITCH_DEGREES  "d9bc177b-1fbe-5724-867a-558e397f2401"
+#define BLE_UUID_TARE_SWITCH  "f7a73029-a679-5de1-8448-ba3d23450f75"
+
 // Bluetooth® Low Energy Battery Service
-BLEService batteryService("180F");
-BLEService rollservice("185A");
-BLEService pitchservice("185B");
-BLEService tareService("19B10000-E8F2-537E-4F6C-D104768A1214");
+BLEService angleMonitorService(BLE_UUID_ANGLE_MONITOR_SERVICE);
 
-// Bluetooth® Low Energy Battery Level Characteristic
-BLEUnsignedCharCharacteristic batteryLevelChar("2A19", BLERead | BLENotify); // standard 16-bit characteristic UUID
-BLEUnsignedCharCharacteristic rollchar("2C08", BLERead | BLENotify); // standard 16-bit characteristic UUID
-BLEUnsignedCharCharacteristic pitchchar("2C09", BLERead | BLENotify); // standard 16-bit characteristic UUID
-BLEByteCharacteristic tareChar("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-// remote clients will be able to get notifications if this characteristic changes
+// Bluetooth® Low Energy Characteristics
+BLEStringCharacteristic batteryVolts(BLE_UUID_BATTERY_VOLTS, BLERead | BLENotify, 20);
+BLEStringCharacteristic rollDegrees(BLE_UUID_ROLL_DEGREES, BLERead | BLENotify, 20);
+BLEStringCharacteristic pitchDegrees(BLE_UUID_PITCH_DEGREES, BLERead | BLENotify, 20);
+BLEByteCharacteristic tareChar(BLE_UUID_TARE_SWITCH, BLERead | BLEWrite);
 
-double oldBatteryV = 0.0;  // last battery level reading from analog input
-int roll = 0;  // roll to be sent
-int pitch = 0;  // pitch to be sent
+float oldBatteryV = 0.0;  // last battery level reading from analog input
+char batteryBuffer[20];
+float roll = 0;  // roll to be sent
+char rollBuffer[20];
+float pitch = 0;  // pitch to be sent
+char pitchBuffer[20];
 float rollRaw = 0.0;  // raw calculated roll
 float pitchRaw = 0.0;  // raw calculated pitch
 float tareRoll = 0.0;  // raw roll value when tared
@@ -79,27 +87,27 @@ void setup()
       Serial.println("IMU OK!");
   }
 
-  BLE.setLocalName("AngleMonitor");
+  // Set advertised local name and service
+  BLE.setDeviceName( "Angle Monitor" );
+  BLE.setLocalName( "Angle Monitor" );
+  BLE.setAdvertisedService( angleMonitorService );
 
-// Configure Battery Monitor Service
-  BLE.setAdvertisedService(batteryService); // add the service UUID
-  batteryService.addCharacteristic(batteryLevelChar); // add the battery level characteristic
-  BLE.addService(batteryService); // Add the battery service
-  batteryLevelChar.writeValue(oldBatteryV); // set initial value for this characteristic
-// Configure roll Monitor Service
-  BLE.setAdvertisedService(rollservice); // add the service UUID
-  rollservice.addCharacteristic(rollchar); // add the battery level characteristic
-  BLE.addService(rollservice); // Add the battery service
-  rollchar.writeValue(roll); // set initial value for this characteristic
-// Configure pitch Monitor Service
-  BLE.setAdvertisedService(pitchservice); // add the service UUID
-  pitchservice.addCharacteristic(pitchchar); // add the battery level characteristic
-  BLE.addService(pitchservice); // Add the battery service
-  pitchchar.writeValue(pitch); // set initial value for this characteristic
-// Configure Tare Service
-  BLE.setAdvertisedService(tareService);
-  tareService.addCharacteristic(tareChar);
-  BLE.addService(tareService);
+  // Add BLE characteristics
+  angleMonitorService.addCharacteristic( batteryVolts );
+  angleMonitorService.addCharacteristic( rollDegrees );
+  angleMonitorService.addCharacteristic( pitchDegrees );
+  angleMonitorService.addCharacteristic( tareChar );
+
+  // Add Service
+  BLE.addService( angleMonitorService );
+
+  // Write initial values
+  dtostrf(oldBatteryV, 4, 2, batteryBuffer);
+  batteryVolts.writeValue(batteryBuffer);
+  dtostrf(roll, 4, 2, rollBuffer);
+  rollDegrees.writeValue(rollBuffer);
+  dtostrf(pitch, 4, 2, pitchBuffer);
+  pitchDegrees.writeValue(pitchBuffer);
   tareChar.writeValue(0);
 
   // start advertising
@@ -171,14 +179,14 @@ void loop()
 void updateBatteryLevel()
 {
   int battery = analogRead(batteryAnalogPin); // read battery adc
-  double batteryV = (battery * 3.3) / 1024 * 1510.0 / 510.0; // calc actual battery volts w/ 3v3 reg and 10bit adc
-  battery = batteryV * 1000; // use 4 digit precision for int mapping to %
-  battery = map(battery, batteryMinV, batteryMaxV, 0, 100); // map volts to %
+  float batteryV = (float(battery) * 3.3) / 1024 * 1510.0 / 510.0; // calc actual battery volts w/ 3v3 reg and 10bit adc
   if (batteryV != oldBatteryV)    // if the battery level has changed
     { 
+      dtostrf(oldBatteryV, 4, 2, batteryBuffer);
+      batteryVolts.writeValue(batteryBuffer);
       Serial.print("Battery Volts: "); // print actual battery volts
-      Serial.println(batteryV);
-      batteryLevelChar.writeValue(battery);  // send the battery %
+      Serial.println(batteryBuffer);
+      batteryVolts.writeValue(batteryBuffer);  // send the battery %
       oldBatteryV = batteryV;           // save the volts for next comparison
     } 
 }
@@ -191,14 +199,16 @@ void updateAngles()
   float accZ = myIMU.readFloatAccelZ();
   rollRaw = atan2(accY, accZ) * 57.2958;
   pitchRaw = atan2(-accX, sqrt(accY * accY + accZ * accZ)) * 57.2958;
-  roll = rollRaw - tareRoll;
-  pitch = pitchRaw - tarePitch;
+  roll = (rollRaw - tareRoll);
+  pitch = (pitchRaw - tarePitch);
+  dtostrf(roll, 4, 2, rollBuffer);
+  dtostrf(pitch, 4, 2, pitchBuffer);
   Serial.print("Roll: "); // print roll angle
-    Serial.println(roll);
-  rollchar.writeValue(roll);  // send the roll value
+    Serial.println(rollBuffer);
+  rollDegrees.writeValue(rollBuffer);
   Serial.print("Pitch: "); // print pitch angle
-    Serial.println(pitch);
-  pitchchar.writeValue(pitch);  // send the pitch value
+    Serial.println(pitchBuffer);
+  pitchDegrees.writeValue(pitchBuffer);
 }
 
 void tareAxis()
